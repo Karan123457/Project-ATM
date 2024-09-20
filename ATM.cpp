9df401d9521a5d49050211c2fda47ac8085da5cc
Karan Kumar <143918192+Karan123457@users.noreply.github.com>
2024-09-20 14:07:52 +0530

    Add files via upload

---
+++ ATM.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+class ATM
+{
+private:
+    int balance;
+public:
+    ATM(int bal){
+        balance = bal;
+    }
+    int getBalance(){
+        return balance;
+    }
+    int withdraw(int amount)
+    {
+        if(amount>balance){
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+    void deposit(int amount){
+        balance += amount;
+    }
+};
+main()
+{
+    ATM atm(1000);
+    int choice,amount,success;
+    char option;
+    do
+    {
+        system("cls");
+        cout<<"1. View Balance"<<endl;
+        cout<<"2. Cash Withdraw"<<endl;
+        cout<<"3. Cash Deposit"<<endl;
+        cout<<"4. Exit"<<endl;
+        cout<<"Enter Your Choice :";
+        cin>>choice;
+
+        switch(choice)
+        {
+        case 1:
+            cout<<"Your Balance: "<<atm.getBalance();
+            break;
+        case 2:
+            cout<<"Your Avalable Balance :"<<atm.getBalance()<<endl;
+            cout<<"Enter the Amount to Withdraw: ";
+            cin>>amount;
+            success = atm.withdraw(amount);
+            if(success)
+            {
+                cout<<"withdraw Successful..."<<endl;
+            }
+            else
+            {
+                cout<<"Insufficient Balance..."<<endl;
+            }
+            break;
+        case 3:
+            cout<<"Enter the Amount to Deposit: ";
+            cin>>amount;
+            atm.deposit(amount);
+            cout<<"Deposite Successful..."<<endl;
+            break;
+        case 4:
+            cout<<"Thanks for using ATM"<<endl;
+            break;
+        default:
+            cout<<"Invalid Choice"<<endl;
+        }
+        cout<<"\nDo You want to try Another Transaction [Yes / No] :";
+        cin>>option;
+    }
+    while(option == 'y'|| option == 'Y');
+    //cout<<"Your Balance: "<<atm.getBalance();
+}
+
